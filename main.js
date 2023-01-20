007e22f9bb6f5a4ea8e975c9e594bb8d72982931
T31K <t31kmunwong@gmail.com>
2023-01-20 11:21:56 +0800

    auto-commit

--- main.js
+++ main.js
@@ -66,14 +66,16 @@ app.whenReady().then(() => {
     }
   });
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('before-quit', () => {
+  Bash.$`osascript -e 'tell application "System Events" to set the autohide of the dock preferences to false'`;
 });
 
 var getTrackInterval;
