04cd433b61ea1c8b6788fb785c1c7f2d84e25523
T31K <t31kmunwong@gmail.com>
2023-01-26 14:09:19 +0800

    auto-commit

---
+++ trial.js
@@ -0,0 +1,10 @@
+let freeTrial = $('#free-trial');
+let purchaseLicense = $('#purchase-license');
+
+freeTrial.on('click', () => {
+  console.log('yes');
+});
+
+purchaseLicense.on('click', () => {
+  console.log('yes');
+});
