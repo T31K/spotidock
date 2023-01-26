a4331a9a6a69bc53fc385c81da8f03a3beb1a785
T31K <t31kmunwong@gmail.com>
2023-01-26 14:11:22 +0800

    auto-commit

--- trial.js
+++ trial.js
@@ -2,9 +2,5 @@ let freeTrial = $('#free-trial');
 let purchaseLicense = $('#purchase-license');
 
 freeTrial.on('click', () => {
-  console.log('yes');
-});
-
-purchaseLicense.on('click', () => {
-  console.log('yes');
+  ipcRenderer.send('mainChannel', { command: 'generateSubToken' });
 });
